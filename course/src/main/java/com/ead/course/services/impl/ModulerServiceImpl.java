7a17815187ee766ff1c58929aecacf148f32a6e8
Ricardo Pereira <rikardupereira@gmail.com>
2023-08-14 15:12:58 -0300

    commit inicial para o micreserviços de Course

---
+++ course/src/main/java/com/ead/course/services/impl/ModulerServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ead.course.services.impl;
+
+import com.ead.course.services.ModulerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ModulerServiceImpl implements ModulerService {
+}
