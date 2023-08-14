752b69268184d9098c4746f272405f8f458659b2
Ricardo Pereira <rikardupereira@gmail.com>
2023-08-14 15:14:45 -0300

    Inserindo Injeção de Dependencias na camada Service

--- course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,8 +1,15 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.repositories.CourserRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseServiceImpl implements CourseService {
+
+    private final CourserRepository courserRepository;
+
+    public CourseServiceImpl(CourserRepository courserRepository) {
+        this.courserRepository = courserRepository;
+    }
 }
--- course/src/main/java/com/ead/course/services/impl/ModulerServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/ModulerServiceImpl.java
@@ -1,8 +1,15 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.repositories.ModuleRespository;
 import com.ead.course.services.ModulerService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ModulerServiceImpl implements ModulerService {
+
+    private final ModuleRespository moduleRespository;
+
+    public ModulerServiceImpl(ModuleRespository moduleRespository) {
+        this.moduleRespository = moduleRespository;
+    }
 }
