aa475fad5ce064d2cfc203509de6ce77e5add946
Ricardo Pereira <rikardupereira@gmail.com>
2023-12-09 18:17:10 -0300

    desenvolvendo

--- authuser/src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -1,20 +1,27 @@
 package com.ead.authuser.controllers;
 
-import com.ead.authuser.models.UserModel;
+import com.ead.authuser.dtos.CourseDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @Log4j2
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class UserCourseController {
 
-    @GetMapping("/users/{userId/courses}")
-    public ResponseEntity<Page<UserModel>> getAllCoursesByUser() {
+    @GetMapping("/users/{userId}/courses")
+    public ResponseEntity<Page<CourseDto>> getAllCoursesByUser(@PageableDefault(page = 0, size = 10, sort = "courseId",
+            direction = Sort.Direction.ASC) Pageable pageable, @PathVariable(value = "userId") UUID userId) {
 
     }
 }
---
+++ authuser/src/main/java/com/ead/authuser/dtos/CourseDto.java
@@ -0,0 +1,17 @@
+package com.ead.authuser.dtos;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class CourseDto {
+
+    private UUID courseId;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private CourseStatus courseStatus;
+    private UUID userInstructor;
+    private CourseLevel courseLevel;
+}
