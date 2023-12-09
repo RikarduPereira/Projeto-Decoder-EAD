710c49b61487bcdba1b7263998da505757d221b9
Ricardo Pereira <rikardupereira@gmail.com>
2023-12-09 19:37:30 -0300

    Criando Rest Template parte 2

--- authuser/src/main/java/com/ead/authuser/clients/UserClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/UserClient.java
@@ -1,14 +1,20 @@
 package com.ead.authuser.clients;
 
 import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Log4j2
@@ -32,12 +38,16 @@ public class UserClient {
         log.info("Request URL: {}", url);
 
         try {
-            log.info("Response Number of Elements: {}", searchResult.size());
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {
+            };
+            ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = Objects.requireNonNull(result.getBody()).getContent();
+            log.debug("Response Number of Elements: {}", searchResult.size());
         } catch (HttpStatusCodeException e) {
             log.error("Error request {}", e);
         }
         log.info("Ending Request / courses userId {} ", userId);
 
-        return null;
+        return new PageImpl<>(searchResult);
     }
 }
