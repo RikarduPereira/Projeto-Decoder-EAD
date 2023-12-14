3118993e442cc8b12867087e06013315cff46d69
Ricardo Pereira <rikardupereira@gmail.com>
2023-12-14 19:26:27 -0300

    Criando Rest Template parte 3

--- authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -4,6 +4,7 @@ import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -20,20 +21,23 @@ import java.util.UUID;
 
 @Log4j2
 @Component
-public class UserClient {
+public class CourseClient {
 
     private final RestTemplate restTemplate;
     private final UtilsService utilsService;
 
+    @Value("${ead.api.url.course}")
+    String REQUEST_URL_COURSE;
 
-    public UserClient(RestTemplate restTemplate, UtilsService utilsService) {
+
+    public CourseClient(RestTemplate restTemplate, UtilsService utilsService) {
         this.restTemplate = restTemplate;
         this.utilsService = utilsService;
     }
 
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDto> searchResult = null;
-        String url = utilsService.createUrl(userId, pageable);
+        String url = REQUEST_URL_COURSE + utilsService.createUrl(userId, pageable);
         log.debug("Request URL: {}", url);
         log.info("Request URL: {}", url);
 
--- authuser/src/main/java/com/ead/authuser/services/impl/UtilServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UtilServiceImpl.java
@@ -1,7 +1,6 @@
 package com.ead.authuser.services.impl;
 
 import com.ead.authuser.services.UtilsService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +9,9 @@ import java.util.UUID;
 @Service
 public class UtilServiceImpl implements UtilsService {
 
-    @Value("${ead.api.url.authuser}")
-    String REQUEST_URI;
 
     public String createUrl(UUID userId, Pageable pageable) {
-        return REQUEST_URI + "/courses?userId" + userId + "&page=" + pageable.getPageNumber() + "&size="
+        return "/courses?userId" + userId + "&page=" + pageable.getPageNumber() + "&size="
                 + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
     }
 }
