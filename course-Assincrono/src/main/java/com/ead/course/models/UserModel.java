3e38a8745a6c603a26c51c212b0861230501cdd6
Ricardo Pereira <rikardupereira@gmail.com>
2023-12-17 17:49:04 -0300

    Mapear State Transfer Pattern com Replicação Parcial de Dados

--- course-Assincrono/src/main/java/com/ead/course/models/UserModel.java
+++ course-Assincrono/src/main/java/com/ead/course/models/UserModel.java
@@ -1,6 +1,7 @@
 package com.ead.course.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -18,9 +19,20 @@ import java.util.UUID;
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserModel implements Serializable {
-
     private static final long serialVersionUID = 1L;
-
+    
     @Id
     private UUID userId;
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+    @Column(nullable = false, length = 150)
+    private String fullName;
+    @Column(nullable = false)
+    private String userStatus;
+    @Column(nullable = false)
+    private String userType;
+    @Column(length = 20)
+    private String cpf;
+    @Column
+    private String imageUrl;
 }
