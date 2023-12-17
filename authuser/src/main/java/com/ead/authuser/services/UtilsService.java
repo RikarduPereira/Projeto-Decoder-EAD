0f4d916a1b6287ecb559577e1ff3a4b702098a0e
Ricardo Pereira <rikardupereira@gmail.com>
2023-12-17 16:49:18 -0300

    Duplicando o projeto para termos exemplos de projetos Sincronos e Assincronos

--- authuser/src/main/java/com/ead/authuser/services/UtilsService.java
+++
@@ -1,9 +0,0 @@
-package com.ead.authuser.services;
-
-import org.springframework.data.domain.Pageable;
-
-import java.util.UUID;
-
-public interface UtilsService {
-    public String createUrl(UUID userId, Pageable pageable);
-}
