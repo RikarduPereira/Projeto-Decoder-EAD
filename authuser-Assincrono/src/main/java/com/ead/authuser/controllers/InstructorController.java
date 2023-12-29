2acba250e28d9b625271ad49020e1fdcc202094e
Ricardo Pereira <rikardupereira@gmail.com>
2023-12-28 21:13:29 -0300

    Authorization Roles para Instructor

--- authuser-Assincrono/src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ authuser-Assincrono/src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -11,6 +11,7 @@ import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -31,6 +32,7 @@ public class InstructorController {
         this.roleService = roleService;
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
         Optional<UserModel> userModelOptional = userService.findById(instructorDto.getUserId());
